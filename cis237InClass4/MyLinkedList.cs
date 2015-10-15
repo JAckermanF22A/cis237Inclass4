93e70c0a3d0fbb027fcd576a4e9567fe255b482e
Jacob Ackerman <xylopitty@gmail.com>
2015-10-15 16:12:10 -0400

    End of Day 1

---
+++ cis237InClass4/MyLinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237InClass4
+{
+    class MyLinkedList
+    {
+        //Private variable to hold the current position of where we are in the linked list
+        private Node current;
+        
+        //A public property that will point to the Head node. (The first one in the list)
+        public Node Head
+        {
+            get;
+            set;
+        }
+
+        //Constructor. It will set the head to null because there is nothing in the list yet.
+        public MyLinkedList()
+        {
+            Head = null;
+        }
+
+        public void Add(string content)
+        {
+            Node node = new Node();
+            node.Data = content;
+
+            if(Head == null)
+            {
+                Head = node;
+            }
+            else
+            {
+                current.Next = node;
+            }
+
+            current = node;
+        }
+
+        public bool delete(int Position)
+        {
+            current = Head;
+
+            if(Position == 1)
+            {
+                Head = current.Next;
+                current.Next = null;
+                current = null;
+                return true;
+            }
+
+            if(Position > 1)
+            {
+                Node tempNode = Head;
+                Node lastNode = null;
+                int count = 0;
+
+                while(tempNode != null)
+                {
+                    if(count == Position -1)
+                    {
+                        lastNode.Next = tempNode.Next;
+
+                        tempNode.Next = null;
+                        return true;
+                    }
+
+                    count++;
+
+                    lastNode = tempNode;
+                    tempNode = tempNode.Next;
+                }
+            }
+            return false;
+
+        }
+    }
+}
--- cis237InClass4/Program.cs
+++ cis237InClass4/Program.cs
@@ -10,6 +10,29 @@ namespace cis237InClass4
     {
         static void Main(string[] args)
         {
+            MyLinkedList myLinkedList = new MyLinkedList();
+
+            myLinkedList.Add("first");
+            myLinkedList.Add("second");
+            myLinkedList.Add("third");
+            myLinkedList.Add("fourth");
+
+            for(Node x = myLinkedList.Head; x !=null; x = x.Next)
+            {
+                Console.WriteLine(x.Data);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            myLinkedList.delete(2);
+            myLinkedList.delete(2);
+
+            for (Node x = myLinkedList.Head; x != null; x = x.Next)
+            {
+                Console.WriteLine(x.Data);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
